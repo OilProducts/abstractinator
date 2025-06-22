6ccaa102fbef0e6b40a69eda33f1d4e3cdd1bef1
Chris <christopher.l.charland@gmail.com>
2025-06-22 10:11:37 -0400

    Use SwiGLU feed-forward layers

--- components/__init__.py
+++ components/__init__.py
@@ -2,3 +2,4 @@ from .vector_quantizer import VectorQuantizer
 from .learned_query_attention import LearnedQueryAttention
 from .utils import token_entropy, entropy_segments, build_segment_queries_mask
 from .hierarchical_autoencoder import HierarchicalAutoencoder
+from .swiglu import SwiGLU
