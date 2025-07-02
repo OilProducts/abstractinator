4be57a7988c0604a9fb9dc828272d5ecfcc67ace
Chris <christopher.l.charland@gmail.com>
2025-07-02 13:59:31 -0400

    Add decoder-only expander with sliding window attention

--- components/__init__.py
+++ components/__init__.py
@@ -3,3 +3,4 @@ from .learned_query_attention import LearnedQueryAttention
 from .utils import token_entropy, entropy_segments, build_segment_queries_mask
 from .hierarchical_autoencoder import HierarchicalAutoencoder
 from .swiglu import SwiGLU
+from .expander import DecoderOnlyExpander
