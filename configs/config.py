914a8b0dc5006e854430a7dd7ecae6aefff961e7
Chris <christopher.l.charland@gmail.com>
2025-07-09 13:45:48 -0400

    Organize configs and add multi-stage examples

--- configs/config.py
+++ configs/config.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from base_config import DEVICE, N_CPU, exp_config as _base_exp_config, ExpConfig
+from configs.base_config import DEVICE, N_CPU, exp_config as _base_exp_config, ExpConfig
 
 # Use defaults from base_config without modification
 exp_config: ExpConfig = deepcopy(_base_exp_config)
