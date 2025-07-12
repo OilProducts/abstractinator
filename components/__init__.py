08375289362c1afaa796af9fe72102de0a3ea66e
Chris <christopher.l.charland@gmail.com>
2025-07-12 14:33:01 -0400

    Expose compressor and transformer

--- components/__init__.py
+++ components/__init__.py
@@ -2,7 +2,9 @@ from .vector_quantizer import VectorQuantizer
 from .learned_query_attention import LearnedQueryAttention
 from .utils import token_entropy, entropy_segments, build_segment_queries_mask
 from .hierarchical_autoencoder import HierarchicalAutoencoder
+from .byte_segment_compressor import ByteSegmentCompressor
 from .swiglu import SwiGLU
 from .expander import DecoderOnlyExpander
 from .tokenizer import ByteLevelTokenizer
 from .checkpoint_utils import save_base_components
+from .code_sequence_transformer import CodeSequenceTransformer
