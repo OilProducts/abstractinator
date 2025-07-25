dc2644c31d085030166fad48914852bf9ba8af99
chris <christopher.l.charland@gmail.com>
2025-07-24 21:14:23 -0400

    modify default config

--- configs/stage2_super_tiny_config.py
+++ configs/stage2_super_tiny_config.py
@@ -20,7 +20,7 @@ exp_config.compressor_level_configs = [
         encoder_ffn_dim_multiplier=4,
         max_seq_len_encoder=4096,
         num_queries=1,
-        codebook_size=2048,
+        codebook_size=8192,
         beta=1.0,
         entropy_delta=0.2,
         entropy_abs_threshold=None,
@@ -28,20 +28,21 @@ exp_config.compressor_level_configs = [
         compression_loss_weight=1.0,
     )
 ]
-exp_config.expander.num_enc_layers = 1
-exp_config.expander.num_dec_layers = 1
-exp_config.aux_lm_loss_weight = 0.1
+exp_config.expander_num_enc_layers = 1
+exp_config.expander_num_dec_layers = 1
+exp_config.aux_lm_loss_weight = 1.0
 exp_config.top_lm_loss_weight = 1.0
 exp_config.top_transformer_config = TopTransformerConfig(
     embed_dim=128,
-    dim=128,
+    dim=256,
     num_layers=8,
     num_heads=8,
     ffn_dim_multiplier=4,
     continuous=True,
 )
 exp_config.learning_rate = 5e-4
-exp_config.gradient_accumulation_steps = 2
+exp_config.batch_size = 64
+exp_config.gradient_accumulation_steps = 1
 exp_config.dataset_name = "roneneldan/TinyStories"
 exp_config.dataset_config = None
 exp_config.sample_prompt_for_generation = (
