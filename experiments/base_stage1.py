45d7870248cc86b095c4bdda3ec3acd82fb73e17
chris <christopher.l.charland@gmail.com>
2025-08-04 10:32:25 -0400

    disable "capture scalar inputs", fix oversight where old kpm was passed instead of padded to the top LM.  enable topLM in base stage1 config, add new (but commented) generate bytes method to hierarchical autoencoder

--- experiments/base_stage1.py
+++ experiments/base_stage1.py
@@ -4,5 +4,5 @@ from configs.base_config import DEVICE, N_CPU, exp_config as _base_exp_config, E
 # Use defaults from base_config without modification
 exp_config: ExpConfig = deepcopy(_base_exp_config)
 
-exp_config.top_transformer_config = None
+# exp_config.top_transformer_config = None
 exp_config.num_epochs=2
